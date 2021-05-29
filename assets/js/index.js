65eb7db2120f22209313b429b4cce5580ff7e529
Azizjon Nigmatjonov <74749706+AzizjonNigmatjonov@users.noreply.github.com>
2021-05-29 16:53:20 +0300

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,23 +48,22 @@ const catalogImgWrapper6 = document.querySelector('.catalogue-btn-6 ');
 // get navbar menu btn
 const navBtn = document.getElementById('barBtn');
 // get navbar list
-const navList = document.getElementById('headerNavList');
+const navList = document.querySelector('.nav-list-second');
 // get nav close btn
 const navCloseBtn = document.getElementById('barCloseBtn');
-navCloseBtn.style.display = 'none';
 // open navbar
-
+navList.style.display = 'none';
 const navBarCloseImg = document.querySelector('.nav-bar__img');
 
 navBtn.addEventListener('click', e => {
     bodyWrapper.style.display = 'block';
-    navList.classList.add('active');
-    navCloseBtn.style.display = 'block';
+    navCloseBtn.style.display = 'inline-block';
     navBtn.style.display = 'none';
+    navList.style.display = 'flex';
 })
 
 navCloseBtn.addEventListener('click', e => {
-    navList.classList.remove('active');
+    navList.style.display = 'none';
     navCloseBtn.style.display = 'none';
     navBtn.style.display = 'block';
 })
@@ -74,7 +73,7 @@ const mainLine = document.querySelector('.main-line')
 window.addEventListener('click', e => {
     if (e.target == mainLine || e.target == bodyWrapper) {
         bodyWrapper.style.display = 'none';
-        navList.classList.remove('active')
+        navList.style.display = 'none';
         navCloseBtn.style.display = 'none';
         navBtn.style.display = 'block';
     }
@@ -272,10 +271,130 @@ var swiper = new Swiper(".mySwiper", {
         el: ".swiper-pagination",
         dynamicBullets: true,
     },
+    autoplay: {
+        delay: 3000,
+    },
+    speed: 600,
+    lazy: true,
+    parallax: true,
+    breakpoints: {
+        1200: {
+            slidesPerView: 4,
+        },
+        1180: {
+            slidesPerView: 3,
+        },       
+        960: {
+            slidesPerView: 3,
+        },
+        930: {
+            slidesPerView: 3,
+        },
+        800: {
+            slidesPerView: 2,
+        },
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+        },
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        }
+        
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    
 });
 
 var swiper = new Swiper(".mySwiper2", {});
+
+
+const catalogueSelectElement = document.querySelector('.catalogue-select');
+
+const catalogueSelectOptionElement = document.querySelectorAll('.catalogue-select option');
+
+
+catalogueSelectOptionElement.forEach(element => {
+    element.addEventListener('click', e => {
+        if (e.target.dataset.target == 'cbuldozer') {
+            if (catalogBuldozerList.id  == e.target.dataset.target) {
+                catalogBuldozerList.classList.add('active')
+                catalogImgWrapper3.classList.add('active');
+            } else {
+                catalogBuldozerList.classList.remove('active');
+                catalogImgWrapper3.classList.remove('active');
+            }
+        } else {
+            catalogBuldozerList.classList.remove('active');
+            catalogImgWrapper3.classList.remove('active');
+        }
+        if (e.target.dataset.target == 'ekskavator'){
+            if (catalogEkskavator.id  == e.target.dataset.target) {
+                catalogEkskavator.classList.add('active');
+                catalogImgWrapper4.classList.add('active');
+            } else {
+                catalogEkskavator.classList.remove('active');
+                catalogImgWrapper4.classList.remove('active');
+            }
+        } else {
+            catalogEkskavator.classList.remove('active');
+            catalogImgWrapper4.classList.remove('active');
+        }
+        if (e.target.dataset.target == 'combine'){
+            if (catalogCombineList.id  == e.target.dataset.target) {
+                catalogCombineList.classList.add('active');
+                catalogImgWrapper5.classList.add('active');
+            } else {
+                catalogCombineList.classList.remove('active');
+                catalogImgWrapper5.classList.remove('active');
+            }
+        } else {
+            catalogCombineList.classList.remove('active');
+            catalogImgWrapper5.classList.remove('active');
+        }
+        if (e.target.dataset.target == 'katki'){
+            if (catalogKatki.id  == e.target.dataset.target) {
+                catalogKatki.classList.add('active');
+                catalogImgWrapper6.classList.add('active');
+            } else {
+                catalogKatki.classList.remove('active');
+                catalogImgWrapper6.classList.remove('active');
+            }
+        } else {
+            catalogKatki.classList.remove('active');
+            catalogImgWrapper6.classList.remove('active');
+        }
+        if (e.target.dataset.target == 'peregruz'){
+            if (catalogPeregruz.id  == e.target.dataset.target) {
+                catalogPeregruz.classList.add('active')
+                catalogImgWrapper2.classList.add('active');
+            } else {
+                catalogPeregruz.classList.remove('active')
+                catalogImgWrapper2.classList.remove('active');
+            }
+        } else {
+            catalogPeregruz.classList.remove('active');
+            catalogImgWrapper2.classList.remove('active');
+        }
+        if (e.target.dataset.target == 'tools'){
+            if (catalogTools.id  == e.target.dataset.target) {
+                catalogTools.classList.add('active');
+                catalogImgWrapper1.classList.add('active');
+            } else {
+                catalogTools.classList.remove('active')
+                catalogImgWrapper1.classList.remove('active');
+            }
+        } else {
+            catalogTools.classList.remove('active');
+            catalogImgWrapper1.classList.remove('active');
+        }
+    })
+})
+
+
+
+AOS.init();
