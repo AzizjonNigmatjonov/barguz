3436892502410e80c5eeccd4279b785508f16579
Azizjon Nigmatjonov <74749706+AzizjonNigmatjonov@users.noreply.github.com>
2021-06-08 22:01:58 +0300

    dadwad

--- assets/js/index.js
+++ assets/js/index.js
@@ -305,82 +305,125 @@ const catalogueSelectElement = document.querySelector('.catalogue-select');
 
 const catalogueSelectOptionElement = document.querySelectorAll('.catalogue-select option');
 
+AOS.init();
 
-catalogueSelectOptionElement.forEach(element => {
-    element.addEventListener('click', e => {
-        if (e.target.dataset.target == 'cbuldozer') {
-            if (catalogBuldozerList.id  == e.target.dataset.target) {
-                catalogBuldozerList.classList.add('active')
-                catalogImgWrapper3.classList.add('active');
+const catalogueSelectList = document.querySelector('.catalogue-select-list');
+const catalogueSelectItem = document.querySelectorAll('.catalogue-select-list li');
+const catalogueSecectText = document.querySelector('.catalogue-selector-text');
+const catalogueSecectTextAffter = document.querySelector('.catalogue-selector-text::after');
+const catalogueSelectSubtext = document.querySelectorAll('.catalogue-selector-subtext');
+const catalogueSelectModal = document.querySelector('.catalogue-select-wrapper');
+const catalogueSelectWrapperModal = document.querySelector('.catalogue-select-header')
+
+catalogueSecectText.classList.remove('active') 
+
+window.addEventListener('click', e => {
+    if (e.target == catalogueSelectModal) {
+        catalogueSelectList.classList.remove('active');
+        catalogueSelectList.classList.add('remove')
+        catalogueSelectModal.style.display = 'none';   
+        catalogueSecectText.classList.remove('active')   
+    }
+})
+
+catalogueSecectText.addEventListener('click', e => {
+    catalogueSelectList.classList.add('active')
+    catalogueSelectList.classList.remove('remove')
+    catalogueSelectModal.style.display = 'block';    
+    catalogueSecectText.classList.add('active') 
+    console.log(catalogueSecectText.textContent);
+    catalogueSelectItem.forEach(element => {
+        element.addEventListener('click', e => {
+            if (e.target.dataset.target == 'cbuldozer') {
+                if (catalogBuldozerList.id  == e.target.dataset.target) {
+                    catalogBuldozerList.classList.add('active')
+                    catalogImgWrapper3.classList.add('active');
+                    catalogueSelectModal.style.display = 'none';  
+                    catalogueSecectText.classList.remove('active')  
+                    catalogueSecectText.textContent = e.target.textContent; 
+                } else {
+                    catalogBuldozerList.classList.remove('active');
+                    catalogImgWrapper3.classList.remove('active');
+                }
             } else {
                 catalogBuldozerList.classList.remove('active');
                 catalogImgWrapper3.classList.remove('active');
             }
-        } else {
-            catalogBuldozerList.classList.remove('active');
-            catalogImgWrapper3.classList.remove('active');
-        }
-        if (e.target.dataset.target == 'ekskavator'){
-            if (catalogEkskavator.id  == e.target.dataset.target) {
-                catalogEkskavator.classList.add('active');
-                catalogImgWrapper4.classList.add('active');
+            if (e.target.dataset.target == 'ekskavator'){
+                if (catalogEkskavator.id  == e.target.dataset.target) {
+                    catalogEkskavator.classList.add('active');
+                    catalogImgWrapper4.classList.add('active');
+                    catalogueSelectModal.style.display = 'none';  
+                    catalogueSecectText.classList.remove('active')   
+                    catalogueSecectText.textContent = e.target.textContent; 
+                } else {
+                    catalogEkskavator.classList.remove('active');
+                    catalogImgWrapper4.classList.remove('active');
+                }
             } else {
                 catalogEkskavator.classList.remove('active');
                 catalogImgWrapper4.classList.remove('active');
             }
-        } else {
-            catalogEkskavator.classList.remove('active');
-            catalogImgWrapper4.classList.remove('active');
-        }
-        if (e.target.dataset.target == 'combine'){
-            if (catalogCombineList.id  == e.target.dataset.target) {
-                catalogCombineList.classList.add('active');
-                catalogImgWrapper5.classList.add('active');
+            if (e.target.dataset.target == 'combine'){
+                if (catalogCombineList.id  == e.target.dataset.target) {
+                    catalogCombineList.classList.add('active');
+                    catalogImgWrapper5.classList.add('active');
+                    catalogueSelectModal.style.display = 'none';  
+                    catalogueSecectText.classList.remove('active')  
+                    catalogueSecectText.textContent = e.target.textContent;  
+                } else {
+                    catalogCombineList.classList.remove('active');
+                    catalogImgWrapper5.classList.remove('active');
+                }
             } else {
                 catalogCombineList.classList.remove('active');
                 catalogImgWrapper5.classList.remove('active');
             }
-        } else {
-            catalogCombineList.classList.remove('active');
-            catalogImgWrapper5.classList.remove('active');
-        }
-        if (e.target.dataset.target == 'katki'){
-            if (catalogKatki.id  == e.target.dataset.target) {
-                catalogKatki.classList.add('active');
-                catalogImgWrapper6.classList.add('active');
+            if (e.target.dataset.target == 'katki'){
+                if (catalogKatki.id  == e.target.dataset.target) {
+                    catalogKatki.classList.add('active');
+                    catalogImgWrapper6.classList.add('active');
+                    catalogueSelectModal.style.display = 'none';  
+                    catalogueSecectText.classList.remove('active')   
+                    catalogueSecectText.textContent = e.target.textContent; 
+                } else {
+                    catalogKatki.classList.remove('active');
+                    catalogImgWrapper6.classList.remove('active');
+                }
             } else {
                 catalogKatki.classList.remove('active');
                 catalogImgWrapper6.classList.remove('active');
             }
-        } else {
-            catalogKatki.classList.remove('active');
-            catalogImgWrapper6.classList.remove('active');
-        }
-        if (e.target.dataset.target == 'peregruz'){
-            if (catalogPeregruz.id  == e.target.dataset.target) {
-                catalogPeregruz.classList.add('active')
-                catalogImgWrapper2.classList.add('active');
+            if (e.target.dataset.target == 'peregruz'){
+                if (catalogPeregruz.id  == e.target.dataset.target) {
+                    catalogPeregruz.classList.add('active')
+                    catalogImgWrapper2.classList.add('active');
+                    catalogueSelectModal.style.display = 'none';  
+                    catalogueSecectText.classList.remove('active')   
+                    catalogueSecectText.textContent = e.target.textContent; 
+                } else {
+                    catalogPeregruz.classList.remove('active')
+                    catalogImgWrapper2.classList.remove('active');
+                }
             } else {
-                catalogPeregruz.classList.remove('active')
+                catalogPeregruz.classList.remove('active');
                 catalogImgWrapper2.classList.remove('active');
             }
-        } else {
-            catalogPeregruz.classList.remove('active');
-            catalogImgWrapper2.classList.remove('active');
-        }
-        if (e.target.dataset.target == 'tools'){
-            if (catalogTools.id  == e.target.dataset.target) {
-                catalogTools.classList.add('active');
-                catalogImgWrapper1.classList.add('active');
+            if (e.target.dataset.target == 'tools'){
+                if (catalogTools.id  == e.target.dataset.target) {
+                    catalogTools.classList.add('active');
+                    catalogImgWrapper1.classList.add('active');
+                    catalogueSelectModal.style.display = 'none';  
+                    catalogueSecectText.classList.remove('active')   
+                    catalogueSecectText.textContent = e.target.textContent; 
+                } else {
+                    catalogTools.classList.remove('active')
+                    catalogImgWrapper1.classList.remove('active');
+                }
             } else {
-                catalogTools.classList.remove('active')
+                catalogTools.classList.remove('active');
                 catalogImgWrapper1.classList.remove('active');
             }
-        } else {
-            catalogTools.classList.remove('active');
-            catalogImgWrapper1.classList.remove('active');
-        }
+        })
     })
 })
-
-AOS.init();
